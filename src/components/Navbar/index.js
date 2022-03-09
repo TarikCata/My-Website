68f4d5b8f0cf092ae681727a6700e6cc48511cb9
TarikCata <83212342+TarikCata@users.noreply.github.com>
2022-03-09 19:53:26 +0100

    fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -26,59 +26,64 @@ function Nav() {
     <>
       <nav className={nav ? "navbar active" : "navbar"}>
         <div className="container">
-          <ul
-            className={click ? "menu-item active" : "menu-item"}
-            onClick={() => setClick(!click)}
-          >
-            <Link
-              to="home"
-              activeClass="active-link"
-              spy={true}
-              smooth={true}
-              offsest={-300}
-              duration={1000}
+          <div className="menu-icon-div">
+            <i
+              className={click ? "fas fa-times" : "fas fa-bars"}
               onClick={() => setClick(!click)}
-              className="menu-li one"
-            >
-              Home
-            </Link>
-            <Link
-              to="about"
-              activeClass="active-link"
-              spy={true}
-              smooth={true}
-              offsest={0}
-              duration={1000}
-              onClick={() => setClick(!click)}
-              className="menu-li two"
-            >
-              About
-            </Link>
-            <Link
-              to="work"
-              activeClass="active-link"
-              spy={true}
-              smooth={true}
-              offsest={-1000}
-              duration={1000}
-              onClick={() => setClick(!click)}
-              className="menu-li three"
-            >
-              Work
-            </Link>
-            <Link
-              to="contact"
-              activeClass="active-link"
-              spy={true}
-              smooth={true}
-              offsest={-700}
-              duration={1000}
-              onClick={() => setClick(!click)}
-              className="menu-li four"
-            >
-              Contact
-            </Link>
-          </ul>
+            ></i>
+          </div>
+          <div className={click ? "div-menu-items active" : "div-menu-items"}>
+            <ul className={click ? "menu-item active" : "menu-item"}>
+              <Link
+                to="home"
+                activeClass="active-link"
+                spy={true}
+                smooth={true}
+                offsest={-300}
+                duration={1000}
+                onClick={() => setClick(!click)}
+                className="menu-li one"
+              >
+                Home
+              </Link>
+              <Link
+                to="about"
+                activeClass="active-link"
+                spy={true}
+                smooth={true}
+                offsest={0}
+                duration={1000}
+                onClick={() => setClick(!click)}
+                className="menu-li two"
+              >
+                About
+              </Link>
+              <Link
+                to="work"
+                activeClass="active-link"
+                spy={true}
+                smooth={true}
+                offsest={-1000}
+                duration={1000}
+                onClick={() => setClick(!click)}
+                className="menu-li three"
+              >
+                Work
+              </Link>
+              <Link
+                to="contact"
+                activeClass="active-link"
+                spy={true}
+                smooth={true}
+                offsest={-700}
+                duration={1000}
+                onClick={() => setClick(!click)}
+                className="menu-li four"
+              >
+                Contact
+              </Link>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
